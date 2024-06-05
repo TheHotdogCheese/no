d1e7e8ea1298c099cbff48b29133136d47251fbb
TheHotdogCheese <147284133+TheHotdogCheese@users.noreply.github.com>
2024-06-05 14:09:08 -0400

    terrible mod

--- no.js
+++ no.js
@@ -1,8 +1,24 @@
 addIngredient("Dwayne Johnson",{
-    shape:"rock",
     color:"#526752",
-    glow:"#00ff00",
-    adj:"glowy",
-    broken:"uranium_dust",
-    hidden:false
+    shape:"sus",
+    adj:"sussy",
+    keywords:"the rock"
+});
+
+addIngredient("A Meaty Gyatt",{ //ID should be lowercase with underscores
+    color:"#DE7E5D", //red
+    type:"gyatt", ///type can be any other ingredient
+    shape:"fruit_bi",
+    hiddent: true
+});
+
+addIngredient("Gyatt",{
+    shape:"fruit_bi",
+    color:"#DE7E5D",
+    adj:"rizzy",
+    keywords:"yummy, sigma, dwayne",
+    reactions: {
+        meat: { set1:"A Meaty Gyatt" },
+        water: {set1: "broth"}
+    }
 });
